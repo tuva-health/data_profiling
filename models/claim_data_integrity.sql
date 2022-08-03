6fa0bd9406ed152bee7169dc1b151b91c24b6094
sarah-tuva <sarah@tuvahealth.com>
2022-08-02 18:00:34 -0600

    adds counts and percentages for general checks

--- models/claim_data_integrity.sql
+++ models/claim_data_integrity.sql
@@ -1,11 +1,14 @@
-{%- set table_list = ( 'eligibility', 'medical_claim' ) -%}
+{{ config(materialized='table') }}
 
+{% set table_list = ( 'eligibility', 'medical_claim' ) -%}
 
 {%- for table_item in table_list %}
 
-    {% set all_columns = adapter.get_columns_in_relation(
-        var( table_item )
-    ) %}
+    {%- set current_table = var( table_item ) -%}
+
+    {%- set all_columns = adapter.get_columns_in_relation(
+        current_table
+    ) -%}
 
     {%- for column_item in all_columns %}
 
@@ -13,15 +16,49 @@
           '{{ table_item }}' as table_name
         , '{{ column_item.name|lower }}' as column_name
         , '{{ column_item.data_type }}' as data_type
+        , (
+            select count(*)::integer as cnt
+            from {{ current_table }}
+          ) as table_total
+        , (
+            select max(len( {{ column_item.name }} ))::integer as cnt
+            from {{ current_table }}
+          ) as max_column_length
+        , (
+            select count(*)::integer as cnt
+            from {{ current_table }}
+            where {{ column_item.name }} is null
+          ) as missing_values
+        , case
+            when table_total =  0 then null
+            else ((missing_values::decimal(18,2) / table_total::decimal(18,2))*100)::decimal(18,2)
+          end as missing_percentage
+        , (
+            select count(*)::integer as cnt
+            from {{ current_table }}
+            where {{ column_item.name }}::varchar = ''
+          ) as blank_values
+        , case
+            when table_total =  0 then null
+            else ((blank_values::decimal(18,2) / table_total::decimal(18,2))*100)::decimal(18,2)
+          end as blank_percentage
+        , (
+            select count(distinct {{ column_item.name }} )::integer as ct
+            from {{ current_table }}
+          ) as unique_values
+        , case
+            when table_total =  0 then null
+            else ((unique_values::decimal(18,2) / table_total::decimal(18,2))*100)::decimal(18, 2)
+          end as unique_percentage
 
-    {%- if not loop.last %}
+    {% if not loop.last -%}
         union all
-    {% endif %}
+    {%- endif -%}
 
-    {%- endfor %}
+    {%- endfor -%}
 
-    {%- if not loop.last %}
+    {% if not loop.last -%}
         union all
-    {% endif %}
+    {%- endif -%}
 
-{%- endfor %}
+{%- endfor -%}
