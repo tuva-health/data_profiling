a166c6a95fc2349fdeaa1aaa95d81b33f2220e6e
sarah-tuva <sarah@tuvahealth.com>
2022-10-19 13:59:44 -0600

    Update seed file and references for claim_type

--- models/claims_input_layer/medical_claim_detail.sql
+++ models/claims_input_layer/medical_claim_detail.sql
@@ -110,44 +110,44 @@ joined as (
         , {{ missing_field_check('medical_claim.claim_start_date') }} as missing_claim_start_date_med
         , {{ missing_field_check('medical_claim.claim_end_date') }} as missing_claim_end_date_med
         , case
-            when medical_claim.claim_type = 'I'
+            when medical_claim.claim_type = 'institutional'
             then {{ missing_field_check('medical_claim.admission_date') }}
             else 0
           end as missing_admission_date_med
         , case
-            when medical_claim.claim_type = 'I'
+            when medical_claim.claim_type = 'institutional'
             then {{ missing_field_check('medical_claim.discharge_date') }}
             else 0
           end as missing_discharge_date_med
         , {{ missing_field_check('medical_claim.claim_type') }} as missing_claim_type_med
         , case
-            when medical_claim.claim_type = 'I'
+            when medical_claim.claim_type = 'institutional'
             then {{ missing_field_check('medical_claim.bill_type_code') }}
             else 0
           end as missing_bill_type_code_med
         , case
-            when medical_claim.claim_type = 'P'
+            when medical_claim.claim_type = 'professional'
             then {{ missing_field_check('medical_claim.place_of_service_code') }}
             else 0
           end as missing_place_of_service_code_med
         , case
-            when medical_claim.claim_type = 'I'
+            when medical_claim.claim_type = 'institutional'
             then {{ missing_field_check('medical_claim.revenue_center_code') }}
             else 0
           end as missing_revenue_center_code_med
         , case
-            when medical_claim.claim_type = 'P'
+            when medical_claim.claim_type = 'professional'
             then {{ missing_field_check('medical_claim.hcpcs_code') }}
             else 0
           end as missing_hcpcs_code_med
         , case
-            when medical_claim.claim_type = 'P'
+            when medical_claim.claim_type = 'professional'
             then {{ missing_field_check('medical_claim.billing_npi') }}
             else 0
           end as missing_billing_npi_med
         , {{ missing_field_check('medical_claim.rendering_npi') }} as missing_rendering_npi_med
         , case
-            when medical_claim.claim_type = 'I'
+            when medical_claim.claim_type = 'institutional'
             then {{ missing_field_check('medical_claim.facility_npi') }}
             else 0
           end as missing_facility_npi_med
@@ -155,19 +155,19 @@ joined as (
         , {{ missing_field_check('medical_claim.paid_amount') }} as missing_paid_amount_med
         , {{ missing_field_check('medical_claim.diagnosis_code_1') }} as missing_diagnosis_code_1_med
         , case
-            when medical_claim.claim_type = 'I'
+            when medical_claim.claim_type = 'institutional'
             then {{ missing_field_check('medical_claim.diagnosis_poa_1') }}
             else 0
           end as missing_diagnosis_poa_1_med
         , {{ valid_claim_date_check('medical_claim.claim_start_date') }} as invalid_claim_start_date_med
         , {{ valid_claim_date_check('medical_claim.claim_end_date') }} as invalid_claim_end_date_med
         , case
-            when medical_claim.claim_type = 'I'
+            when medical_claim.claim_type = 'institutional'
             then {{ valid_claim_date_check('medical_claim.admission_date') }}
             else 0
           end as invalid_admission_date_med
         , case
-            when medical_claim.claim_type = 'I'
+            when medical_claim.claim_type = 'institutional'
             then {{ valid_claim_date_check('medical_claim.discharge_date') }}
             else 0
           end as invalid_discharge_date_med
@@ -188,7 +188,7 @@ joined as (
           end as invalid_discharge_before_admission_med
         , case
             when medical_claim.claim_type is null then 0
-            when seed_claim_type.code is not null then 0
+            when seed_claim_type.description is not null then 0
             else 1
           end as invalid_claim_type_med
         , case
@@ -236,7 +236,7 @@ joined as (
          left join seed_bill_type
             on medical_claim.bill_type_code = seed_bill_type.code
          left join seed_claim_type
-            on medical_claim.claim_type = seed_claim_type.code
+            on medical_claim.claim_type = seed_claim_type.description
          left join seed_discharge_disposition
             on medical_claim.discharge_disposition_code = seed_discharge_disposition.discharge_disposition_code
          left join seed_icd_10_cm
