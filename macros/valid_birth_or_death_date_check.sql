3a49d9d3848c16eaea61fe0b23337e08d6b849af
sarah-tuva <sarah@tuvahealth.com>
2022-10-26 15:21:58 -0600

    Add safe casting logic for better date testing

--- macros/valid_birth_or_death_date_check.sql
+++ macros/valid_birth_or_death_date_check.sql
@@ -3,14 +3,44 @@
     comparing it to a realistic date range.
 #}
 
-{% macro valid_birth_or_death_date_check(column_name) %}
+{%- macro valid_birth_or_death_date_check(column_name) -%}
 
-case
-  when {{ column_name }} is null then 0
-  when {{ column_name }} is not null
-    and {{ column_name }} between '1900-01-01' and {{ current_date_or_timestamp('date') }}
-    then 0
-  else 1
-end
+    {{ return(adapter.dispatch('valid_birth_or_death_date_check')(column_name)) }}
 
-{% endmacro %}
+{%- endmacro -%}
+
+{%- macro bigquery__valid_birth_or_death_date_check(column_name) -%}
+
+    case
+      when {{ column_name }} is null then 0
+      when safe_cast({{ column_name }} as date) is not null
+        and safe_cast({{ column_name }} as date) between '1900-01-01' and {{ current_date_or_timestamp('date') }}
+        then 0
+      else 1
+    end
+
+{%- endmacro -%}
+
+{%- macro default__valid_birth_or_death_date_check(column_name) -%}
+
+    case
+      when {{ column_name }} is null then 0
+      when try_cast({{ column_name }} as date) is not null
+        and try_cast({{ column_name }} as date) between '1900-01-01' and {{ current_date_or_timestamp('date') }}
+        then 0
+      else 1
+    end
+
+{%- endmacro -%}
+
+{%- macro snowflake__valid_birth_or_death_date_check(column_name) -%}
+
+    case
+      when {{ column_name }} is null then 0
+      when try_cast({{ column_name }} as date) is not null
+        and try_cast({{ column_name }} as date) between '1900-01-01' and {{ current_date_or_timestamp('date') }}
+        then 0
+      else 1
+    end
+
+{%- endmacro -%}
--- macros/valid_claim_date_check.sql
+++ macros/valid_claim_date_check.sql
@@ -3,14 +3,44 @@
     comparing it to a realistic date range.
 #}
 
-{% macro valid_claim_date_check(column_name) %}
+{%- macro valid_claim_date_check(column_name) -%}
 
-case
-  when {{ column_name }} is null then 0
-  when {{ column_name }} is not null
-    and {{ column_name }} between '2000-01-01' and {{ current_date_or_timestamp('date') }}
-    then 0
-  else 1
-end
+    {{ return(adapter.dispatch('valid_claim_date_check')(column_name)) }}
 
 {% endmacro %}
+
+{%- macro bigquery__valid_claim_date_check(column_name) -%}
+
+    case
+      when {{ column_name }} is null then 0
+      when safe_cast({{ column_name }} as date) is not null
+        and safe_cast({{ column_name }} as date) between '2000-01-01' and {{ current_date_or_timestamp('date') }}
+        then 0
+      else 1
+    end
+
+{%- endmacro -%}
+
+{%- macro default__valid_claim_date_check(column_name) -%}
+
+    case
+      when {{ column_name }} is null then 0
+      when try_cast({{ column_name }} as date) is not null
+        and try_cast({{ column_name }} as date) between '2000-01-01' and {{ current_date_or_timestamp('date') }}
+        then 0
+      else 1
+    end
+
+{%- endmacro -%}
+
+{%- macro snowflake__valid_claim_date_check(column_name) -%}
+
+    case
+      when {{ column_name }} is null then 0
+      when try_cast({{ column_name }} as date) is not null
+        and try_cast({{ column_name }} as date) between '2000-01-01' and {{ current_date_or_timestamp('date') }}
+        then 0
+      else 1
+    end
+
+{%- endmacro -%}
