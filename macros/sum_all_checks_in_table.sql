198a000e57940b11c0db3a8b75c9052251ec1fda
tuvaforrest <forrest@tuvahealth.com>
2022-11-30 11:04:01 -0500

    updated relat variable name to relation to be more explicit

--- macros/sum_all_checks_in_table.sql
+++ macros/sum_all_checks_in_table.sql
@@ -7,21 +7,21 @@
 #}
 
 
-{% macro sum_all_checks_in_table(relat, column_list) %}
+{% macro sum_all_checks_in_table(relation, column_list) %}
 
 
     {%- set all_columns = adapter.get_columns_in_relation(
-        relat
+        relation
     ) -%}
 
     {%- for column_item in all_columns
         if column_item.name.lower() in column_list %}
 
         select
-              '{{ relat.identifier }}' as table_name
+              '{{ relation.identifier }}' as table_name
             , '{{ column_item.name|lower }}' as test_name
             , sum( {{ column_item.name }} ) as test_fail_numerator
-        from {{ relat }}
+        from {{ relation }}
 
         {% if not loop.last -%}
             union all
