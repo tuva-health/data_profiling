52f3ee464d0e8a3023f8285188fa0b4c023b529c
tuvaforrest <forrest@tuvahealth.com>
2022-12-07 16:28:13 -0500

    updates for modularization

--- integration_tests/models/pharmacy_claim.sql
+++ integration_tests/models/pharmacy_claim.sql
@@ -1,1 +1,18 @@
-select * from {{source('claims_input','pharmacy_claim')}}
+select
+    cast(null as varchar) as claim_id
+    , cast(null as varchar) as claim_line_number
+    , cast(null as varchar) as patient_id
+    , cast(null as varchar) as member_id
+    , cast(null as varchar) as prescribing_provider_npi
+    , cast(null as varchar) as dispensing_provider_npi
+    , cast(null as varchar) as dispensing_date
+    , cast(null as varchar) as ndc_code
+    , cast(null as varchar) as ndc_description
+    , cast(null as varchar) as quantity
+    , cast(null as int) as days_supply
+    , cast(null as varchar) as refills
+    , cast(null as date) as paid_date
+    , cast(null as numeric) as paid_amount
+    , cast(null as numeric) as allowed_amount
+    , cast(null as varchar) as data_source
+where 1=0
